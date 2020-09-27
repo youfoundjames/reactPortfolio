aad5daa9c87737c5cdb7ae9352023eedfd3017d2
James Cook <63876549+jamescook98@users.noreply.github.com>
2020-09-27 11:26:04 -0500

    add router

---
+++ src/components/Error.js
@@ -0,0 +1,11 @@
+import React from 'react';
+ 
+const Error = () => {
+    return (
+       <div>
+          <p>Computer error. Virus equals very yes.</p>
+       </div>
+    );
+}
+ 
+export default Error;
---
+++ src/components/Portfolio.js
@@ -0,0 +1,12 @@
+import React from 'react';
+ 
+const Portfolio = () => {
+    return (
+       <div>
+          <h1>Portfolio</h1>
+          <p>Portfolio page body content</p>
+       </div>
+    );
+}
+ 
+export default Portfolio;
