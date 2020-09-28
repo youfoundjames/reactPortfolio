154eb11beeecc1063d198b5342276db44b16fbf9
James Cook <63876549+jamescook98@users.noreply.github.com>
2020-09-28 15:56:54 -0500

    engoodening

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,30 @@
 import React from 'react';
 import './style.css';
+import sax from '../assets/sax.png';
 
 const home = () => {
     return (
-       <div className="main">
-          <h1>Home</h1>
-           <p>Home page body content</p>
-       </div>
+        <div className="main">
+            <h1>About Me</h1>
+            <div>
+                <div className="row">
+                    <div className="columnPadded">
+                        <img src={sax} style={{ width: "100%" }} alt="James Cook plsaxophone at the Green Mill"
+                        />
+                    </div>
+                    <div className="columnPadded">
+                        I am a full-stack web developer originally from Chicago, IL, currently based in Brooklyn, NY. I am fluent in Javascript, React.js, CSS, MySQL, Node/Express, and MongoDB, among other technologies. In 2020, I recieved a certificate in full-stack web development from Columbia University in New York.<br /><br /><br />
+Contact me at:<br />
+            <i>cookjamesarthur [at] gmail [dot] com</i><br /><br /><br />
+                        <a href="https://github.com/jamescook98">GitHub</a>
+                        <a href="https://www.linkedin.com/in/james-cook-b0b53a145/">LinkedIn</a>
+                        <a href="https://docs.google.com/document/d/1PrDKIx2ZXnkgMQWEaMTh8jR-U3YJDs2ImqE5qgw0TtM/edit?usp=sharing">Resume/CV</a>
+                    </div>
+                </div>
+
+            </div>
+        </div>
     );
 }
- 
+
 export default home;
--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,12 +1,84 @@
 import React from 'react';
- 
+import peregrine from '../assets/peregrine.png';
+import shotglass from '../assets/shotglass.png';
+import weather from '../assets/weather.png';
+import workday from '../assets/workday.png';
+import password from '../assets/password.png';
+
 const Portfolio = () => {
-    return (
-       <div className="main">
-          <h1>Portfolio</h1>
-          <p>Portfolio page body content</p>
-       </div>
-    );
+   return (
+      <div className="main">
+         <h1>Portfolio</h1>
+
+         <div className="row">
+            <div className="column">
+               <img className="portfolioImg" src={peregrine} alt="demo of peregrine travel app" />
+            </div>
+            <div className="column">
+               <h3><i>PEREGRINE</i></h3>
+               This application provides an insider's look at travel, helping people to find niche spots with authentic experiences. Those who want to participate as reviewers can login and recommend their own favorite locations. <br /><br />
+               <a className="portfolio-link" href="https://peregrine-travel.herokuapp.com/">Deployed application</a>
+               <a className="plinksmall" href="https://github.com/jamescook98/peregrine">GitHub</a>
+            </div>
+         </div>
+
+         <br /><br />
+
+         <div className="row">
+            <div className="column">
+               <img className="portfolioImg" src={shotglass} alt="demo of shotglass meditation app" />
+            </div>
+            <div className="column">
+               <h3><i>SHOTGLASS MEDITATION</i></h3>
+               ShotGlass is a simple meditation app which allows the user to tailor their own timed meditative experience by selecting from a pool of randomly-generated affirmations and audiovisual themes. <br /><br />
+               <a className="portfolio-link" href="https://bubblyrobot.github.io/teamrepo/">Deployed application</a>
+               <a className="plinksmall" href="https://github.com/jamescook98/shotglassmeditation">GitHub</a>
+            </div>
+         </div>
+
+         <br /><br />
+
+         <div className="row">
+            <div className="column">
+               <img className="portfolioImg" src={weather} alt="demo of weather dashboard app" />
+            </div>
+            <div className="column">
+               <h3><i>WEATHER DASHBOARD</i></h3>
+               Search for a city to see real-time local weather information as well as a five day forecast. Uses OpenWeatherMap API.<br /><br />
+               <a className="portfolio-link" href="https://jamescook98.github.io/weatherdashboard">Deployed application</a>
+               <a className="plinksmall" href="https://github.com/jamescook98/weatherdashboard">GitHub</a>
+            </div>
+         </div>
+
+         <br /><br />
+
+         <div className="row">
+            <div className="column">
+               <img className="portfolioImg" src={workday} alt="demo of workday planner app" />
+            </div>
+            <div className="column">
+               <h3><i>DAY PLANNER</i></h3>
+               Displays current day schedule to add hourly events. Events are color-coded by time of day: past hour is in gray, current hour is in red, future hours are in green. Uses moment.js. My first project using jQuery.<br/><br/>
+               <a className="portfolio-link" href="https://jamescook98.github.io/dayplanner">Deployed application</a>
+               <a className="plinksmall" href="https://github.com/jamescook98/dayplanner">GitHub</a>
+            </div>
+         </div>
+
+         <br /><br />
+
+         <div className="row">
+            <div className="column">
+               <img className="portfolioImg" src={password} alt="demo of password generator app" />
+            </div>
+            <div className="column">
+               <h3><i>PASSWORD GENERATOR</i></h3>
+               A simple Javascript application that generates a random password based on user-selected criteria. User selects which characters should be present in the password, as well as the length of the password, and upon hitting the "generate" button a random password meeting these standards will be printed to the screen.<br/><br/>
+               <a className="portfolio-link" href="https://jamescook98.github.io/passwordgenerator">Deployed application</a>
+               <a className="plinksmall" href="https://github.com/jamescook98/passwordgenerator">GitHub</a>
+            </div>
+         </div>
+      </div>
+   );
 }
- 
+
 export default Portfolio;
