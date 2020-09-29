cc417ca1d3161fcb130b67f28dea30a4fea6fcaa
James Cook <63876549+jamescook98@users.noreply.github.com>
2020-09-28 21:07:31 -0500

    contact form. refactor. css

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,12 +1,28 @@
 import React from 'react';
- 
+
 const Contact = () => {
-    return (
-       <div className="main">
-          <h1>Contact</h1>
-          <p>Contact page body content</p>
-       </div>
-    );
+   return (
+      <div className="main">
+         <h1>Contact</h1>
+         <div class="form-group">
+            <label>Name:</label><br />
+            <input type="text" class="form-control" id="exampleInputPassword1" placeholder="John Smith" /><br /><br />
+         </div>
+
+         <div class="form-group">
+            <label for="exampleInputEmail1">Email address:</label><br />
+            <input type="email" class="form-control" id="exampleInputEmail1" placeholder="johnsmith@ymail.fun" /><br /><br />
+         </div>
+
+         <div class="form-group">
+            <label for="exampleInputMessage1">Message:</label><br />
+            <textarea class="form-control" id="exampleInputMessage1" rows="6"
+               placeholder="Your message here"></textarea><br /><br />
+         </div>
+
+         <button type="button" class="btn btn-primary" id="submitBtn">Submit</button>
+      </div>
+   );
 }
- 
+
 export default Contact;
